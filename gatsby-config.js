8e80c04dfa03e1a43c642276e1f764c534ba593a
Jason A. Wise <jasonandrewwise@gmail.com>
2022-12-24 10:36:49 -0500

    adding fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,5 +123,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Hanken Grotesk`,
+            file: `https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap`
+          }
+        ],
+      },
+    },
   ],
 }
