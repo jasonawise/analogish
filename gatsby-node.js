87a6551079be8b9c42b7c57183b4ba5902ab6d46
Jason A. Wise <jasonandrewwise@gmail.com>
2022-12-24 10:53:33 -0500

    adding the ability for tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,6 +25,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           fields {
             slug
           }
+          frontmatter {
+            tags
+          }
+        }
+      }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: { frontmatter: { tags: SELECT }}) {
+          fieldValue
         }
       }
     }
@@ -60,6 +68,20 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       })
     })
   }
+
+   // Extract tag data from query
+   const tags = result.data.tagsGroup.group
+
+   // Make tag pages
+   tags.forEach(tag => {
+     createPage({
+       path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+       component: tagTemplate,
+       context: {
+         tag: tag.fieldValue,
+       },
+     })
+   })
 }
 
 /**
